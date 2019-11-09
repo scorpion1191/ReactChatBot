dcc4805d3762b9156c3aaa8e72ed0a11f870e188
subbu <subbu1191@gmail.com>
2019-11-09 16:04:15 +0530

    spellcheck

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleProjectId:process.env.GOOGLE_PROJECT_ID,
+    googleProjectID:process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID:process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode:process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail:process.env.GOOGLE_CLIENT_EMAIL,
