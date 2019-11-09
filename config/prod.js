ef4d5c34a7ae21d761446b188bdf16ce86cf4dd5
subbu <subbu1191@gmail.com>
2019-11-09 15:57:33 +0530

    adding process variable

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     googleProjectId:process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID:process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode:process.env.DIALOGFLOW_LANGUAGE_CODE,
-    googleClientEmail:GOOGLE_CLIENT_EMAIL,
+    googleClientEmail:process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivatKey:process.env.GOOGLE_PRIVATE_KEY,
 }
